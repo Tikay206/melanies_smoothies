428bd4f4dc54f3579383f21bbbf1e72194b51221
Tikay206 <tinku.jose@gmail.com>
2025-12-20 18:05:08 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ if ingredients_list:
 
 # New section to display smoothiefroot nutrition information
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
 
         

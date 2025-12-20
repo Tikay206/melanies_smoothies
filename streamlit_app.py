68d9360f6f98164d232e75820d6ee8714a991511
Tikay206 <tinku.jose@gmail.com>
2025-12-20 16:15:28 +1300

    Update streamlit_app.py
    editing

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 
 conn = st.connection("snowflake")
 df = conn.query("SELECT * FROM smoothies.public.fruit_options", ttl="10m")

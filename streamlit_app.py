f6afd353dcb5d942a16e78e7ac3747813bedc769
Tikay206 <tinku.jose@gmail.com>
2025-12-20 16:37:45 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # Import python packages
+import os
 import streamlit as st
 from snowflake.snowpark.functions import col
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark import Session
 session = get_active_session()
 

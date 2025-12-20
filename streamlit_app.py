8587ec428401d9c3dd0f0128c44f1387247967a0
Tikay206 <tinku.jose@gmail.com>
2025-12-21 11:13:38 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ conn = st.connection("snowflake")
 #session = cnx.session()
 import requests
 import pandas as pd
-from pandas import DataFrame, Series
+from pandas import DataFrame
 
 conn = st.connection("snowflake")
 df = conn.query("SELECT * FROM smoothies.public.fruit_options;", ttl="10m")
